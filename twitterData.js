2a3a131e9144f7310375ff453ad5ab8a8dce3b3a
Jspsun <Jspsun@gmail.com>
2016-11-13 04:10:16 -0500

    a

--- twitterData.js
+++ twitterData.js
@@ -12,6 +12,7 @@ var client = new Twitter({
 });
 
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res){
     res.render("search");
