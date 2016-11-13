96e15d52c22ee831981731e3ad6aae305f2688b5
Jspsun <Jspsun@gmail.com>
2016-11-13 03:45:45 -0500

    a

--- twitterData.js
+++ twitterData.js
@@ -11,6 +11,8 @@ var client = new Twitter({
   access_token_secret: 'hlIpItCXae7tUfT6UmIR9eOz5lzksz5tLzdzTR2mCD8Ui'
 });
 
+//NODE
+app.set('port', (process.env.PORT || 5000));
 
 app.get("/", function(req, res){
     res.render("search");
@@ -23,8 +25,12 @@ app.get("/results", function(req, res){
 
     });
 })
+//NODE
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
-app.listen(8000, 'localhost', function(){
-    console.log("server started");
+// app.listen(8000, 'localhost', function(){
+//     console.log("server started");
 
 });
