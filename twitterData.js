921e3734e7595049ed65653f01be74a47805bb46
Jspsun <Jspsun@gmail.com>
2016-11-13 06:55:12 -0500

    added favicon

--- twitterData.js
+++ twitterData.js
@@ -16,6 +16,8 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
+
 app.get("/", function(req, res){
     res.render("search");
 })
