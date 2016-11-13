0ca53a350e59804b723dcdbebdc6b2cf325431dd
Jspsun <Jspsun@gmail.com>
2016-11-13 03:47:31 -0500

    a

--- twitterData.js
+++ twitterData.js
@@ -14,6 +14,16 @@ var client = new Twitter({
 //NODE
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
 app.get("/", function(req, res){
     res.render("search");
 })
